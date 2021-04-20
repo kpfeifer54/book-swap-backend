ebdfdd82fa792af2e04cff435550682ff818fec7
kpfeifer54 <krysta.pfeifer@gmail.com>
2021-04-20 08:34:52 -0400

    minor url tweak

--- book_swap/urls.py
+++ book_swap/urls.py
@@ -19,7 +19,7 @@ from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('books.urls')),
+    path('books/', include('books.urls')),
     path('token-auth/', obtain_jwt_token),
     path('core/', include('core.urls'))
 ]
