4bda34ed05eada057296d5ec4aa2c7ed10b47860
kpfeifer54 <krysta.pfeifer@gmail.com>
2021-04-19 22:55:16 -0400

    cors

--- book_swap/settings.py
+++ book_swap/settings.py
@@ -129,3 +129,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
+
+CORS_ORIGIN_WHITELIST = ['https://localhost:3000', 'http://localhost:3000']
